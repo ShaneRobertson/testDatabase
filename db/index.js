232f7ed1177a3af547e6a9371a9ac5a89fe181ce
Shane Robertson <sjrobertson0812@gmail.com>
2020-12-05 10:54:45 -0500

    end of part 1

--- db/index.js
+++ db/index.js
@@ -1,1 +1,32 @@
-const hello = "hello"
+const {Client} = require('pg');
+
+const client = new Client('postgres://postgres:ganon3422@localhost:5432/juiceboz-dev')
+
+const getAllUsers = async () => {
+        const {rows} = await client.query(`
+            SELECT id, username 
+            FROM users;
+        `)
+        return rows
+}
+
+const createUser = async ({username, password}) =>{
+    try {
+        const {rows} = await client.query(`
+        INSERT INTO users(username, password)
+        VALUES ($1, $2)
+        ON CONFLICT (username) DO NOTHING
+        RETURNING *;
+        `, [username, password])
+        
+        return rows
+    } catch (error) {
+        throw error
+    }
+}
+
+module.exports = {
+    client,
+    getAllUsers,
+    createUser
+}
